d1ee6da33c41491304eaf0bb6417c5ae6b5dad84
4hiziri <meirvg@gmail.com>
2017-12-11 14:30:37 +0900

    Comment out not implemented function

--- my-logger-mod.c
+++ my-logger-mod.c
@@ -22,16 +22,16 @@ static struct device* mkl_dev = NULL;
 static char *module_name = DEVICE_NAME;
 module_param(module_name, charp, S_IRUGO);
 
-static int dev_open(struct inode *, struct file *);
-static int dev_release(struct inode *, struct file *);
-static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
-static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+/* static int dev_open(struct inode *, struct file *); */
+/* static int dev_release(struct inode *, struct file *); */
+/* static ssize_t dev_read(struct file *, char *, size_t, loff_t *); */
+/* static ssize_t dev_write(struct file *, const char *, size_t, loff_t *); */
 
 static struct file_operations fops = {
-  .open = dev_open,
-  .read = dev_read,
-  .write = dev_write,
-  .release = dev_release,
+  /* .open = dev_open, */
+  /* .read = dev_read, */
+  /* .write = dev_write, */
+  /* .release = dev_release, */
 };
 
 static int __init my_key_logger_init(void){
